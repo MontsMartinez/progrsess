efba359126fa12ea803b70a78a2cb3073e9def13
TaniaMontserrat <montserrat.ma.es.42@gmail.com>
2024-06-06 20:40:09 -0600

    Auto-generated commit

--- src/app/pipes/videopipe.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { VideopipePipe } from './videopipe.pipe';
-
-describe('VideopipePipe', () => {
-  it('create an instance', () => {
-    const pipe = new VideopipePipe();
-    expect(pipe).toBeTruthy();
-  });
-});
