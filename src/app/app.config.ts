e8b6676ff0e10a7d13d0d1755fb3dbcd80b7e117
TaniaMontserrat <montserrat.ma.es.42@gmail.com>
2024-06-06 20:21:11 -0600

    icons

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -10,5 +10,8 @@ export const appConfig: ApplicationConfig = {
   providers: [provideRouter(routes), provideClientHydration(), provideHttpClient(), provideServiceWorker('ngsw-worker.js', {
         enabled: !isDevMode(),
         registrationStrategy: 'registerWhenStable:30000'
+    }), provideServiceWorker('ngsw-worker.js', {
+        enabled: !isDevMode(),
+        registrationStrategy: 'registerWhenStable:30000'
     })]
 };
